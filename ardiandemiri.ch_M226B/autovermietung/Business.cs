d75c2b4aa8ef43f30edf2c24f3772645c6345703
Ardian <ardiandemiri11@gmail.com>
2020-08-17 22:28:10 +0200

    Class Business With Attributes

---
+++ ardiandemiri.ch_M226B/autovermietung/Business.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace autovermietung
+{
+    public class Business
+    {
+        private string _businessName;
+        private string _website;
+
+        public Business(string Name, string Website)
+        {
+            _businessName = Name;
+            _website = Website;
+        }
+
+        public Business() { }
+    }
+}
