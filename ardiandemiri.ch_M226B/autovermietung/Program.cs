cb2543b10e83ae4685fbeb864466f1d04702d484
Ardian <ardiandemiri11@gmail.com>
2020-08-17 21:54:38 +0200

    * Program.cs:
    * autovermietung.csproj: Class Car Created

--- ardiandemiri.ch_M226B/autovermietung/Program.cs
+++ ardiandemiri.ch_M226B/autovermietung/Program.cs
@@ -6,8 +6,8 @@ namespace autovermietung
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Hello Test");
+            
         }
+
     }
 }
