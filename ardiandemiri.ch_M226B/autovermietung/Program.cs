f961992a022cbe61d9756b58f5fd979ae6f58371
Ardian <ardiandemiri11@gmail.com>
2020-08-17 21:18:05 +0200

    Commit Test

--- ardiandemiri.ch_M226B/autovermietung/Program.cs
+++ ardiandemiri.ch_M226B/autovermietung/Program.cs
@@ -7,6 +7,7 @@ namespace autovermietung
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("Hello Test");
         }
     }
 }
