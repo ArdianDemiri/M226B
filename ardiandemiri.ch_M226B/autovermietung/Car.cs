6878ee31ae75cacf5798182bb67767db29fb4b82
Ardian <ardiandemiri11@gmail.com>
2020-08-17 22:20:21 +0200

    Updated Class Car With Attributes

--- ardiandemiri.ch_M226B/autovermietung/Car.cs
+++ ardiandemiri.ch_M226B/autovermietung/Car.cs
@@ -4,10 +4,19 @@ namespace autovermietung
     public class Car
     {
         private int _carId;
+        private string _brand;
+        private string _model;
+        private string _licensePlate;
 
-        public Car()
+
+        public Car(int ID, string Brand, string Model, string LicensePlate)
         {
-
+            _carId = ID;
+            _brand = Brand;
+            _model = Model;
+            _licensePlate = LicensePlate;
         }
+
+        public Car() { }
     }
 }
