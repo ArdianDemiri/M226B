7e3e2dad2372a9dc37f84db306a0600e7d930b1d
Ardian Demiri <ademiri@online.gibz.ch>
2020-08-23 22:56:44 +0200

    * MechanicStaff.cs:
    * autovermietung.csproj: MechanicStaff Class

---
+++ ardiandemiri.ch_M226B/autovermietung/MechanicStaff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace autovermietung
+{
+    public class MechanicStaff : Staff
+    {
+        protected int _mechanicID;
+
+        public MechanicStaff(int MechanicID, int ID, Location location,
+            string Name, string Surname, string PhoneNumber)
+            : base(ID, location, Name, Surname, PhoneNumber)
+        {
+            _mechanicID = MechanicID;
+            _clientID = ID;
+            _location = location;
+            _name = Name;
+            _surname = Surname;
+            _phoneNumber = PhoneNumber;
+        }
+
+        public MechanicStaff() { }
+    }
+}
