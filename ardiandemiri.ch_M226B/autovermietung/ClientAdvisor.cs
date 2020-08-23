ed48d3540f27f0862a5a686737086f898fdee0aa
Ardian Demiri <ademiri@online.gibz.ch>
2020-08-23 22:42:06 +0200

    * ClientAdvisor.cs: * autovermietung.csproj:
    * Staff.cs: ClientAdvisor Class

---
+++ ardiandemiri.ch_M226B/autovermietung/ClientAdvisor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace autovermietung
+{
+    public class ClientAdvisor : Staff
+    {
+        protected List<Client> _clients = new List<Client>();
+
+        public ClientAdvisor(List<Client> clients, int ID, Location location,
+            string Name, string Surname, string PhoneNumber)
+            : base(ID, location, Name, Surname, PhoneNumber)
+        {
+            _clients = clients;
+            _clientID = ID;
+            _location = location;
+            _name = Name;
+            _surname = Surname;
+            _phoneNumber = PhoneNumber;
+        }
+
+        public ClientAdvisor() { }
+    }
+}
--- ardiandemiri.ch_M226B/autovermietung/Staff.cs
+++ ardiandemiri.ch_M226B/autovermietung/Staff.cs
@@ -3,8 +3,21 @@ namespace autovermietung
 {
     public class Staff
     {
-        public Staff()
+        protected int _clientID;
+        protected Location _location;
+        protected string _name;
+        protected string _surname;
+        protected string _phoneNumber;
+
+        public Staff(int ID, Location location, string Name, string Surname, string PhoneNumber)
         {
+            _clientID = ID;
+            _location = location;
+            _name = Name;
+            _surname = Surname;
+            _phoneNumber = PhoneNumber;
         }
+
+        public Staff() { }
     }
 }
