28b70260a405cca251a1a468017282ff72a87414
Ardian Demiri <ademiri@online.gibz.ch>
2020-08-23 22:48:40 +0200

    * CleaningStaff.cs:
    * autovermietung.csproj: Cleaningstaff Class

---
+++ ardiandemiri.ch_M226B/autovermietung/CleaningStaff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace autovermietung
+{
+    public class CleaningStaff : Staff
+    {
+        protected int _cleaningID;
+
+        public CleaningStaff(int CleaningID, int ID, Location location,
+            string Name, string Surname, string PhoneNumber)
+            : base(ID, location, Name, Surname, PhoneNumber)
+        {
+            _cleaningID = CleaningID;
+            _clientID = ID;
+            _location = location;
+            _name = Name;
+            _surname = Surname;
+            _phoneNumber = PhoneNumber;
+        }
+
+        public CleaningStaff() { }
+    }
+}
