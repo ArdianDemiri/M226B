b10c71ddda83496bfd776387193487212e269365
Ardian Demiri <ademiri@online.gibz.ch>
2020-08-23 22:23:22 +0200

    * BusinessClient.cs:
    * autovermietung.csproj: BusinessClient

---
+++ ardiandemiri.ch_M226B/autovermietung/BusinessClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace autovermietung
+{
+    public class BusinessClient : Client
+    {
+        protected string _businessName;
+
+        public BusinessClient(int ID, string Name, string Surname, string PhoneNumber,
+            string Street, string City, int ZipCode, string Country, string BusinessName)
+            : base(ID, Name, Surname, PhoneNumber, Street, City, ZipCode, Country)
+        {
+            _clientID = ID;
+            _name = Name;
+            _surname = Surname;
+            _phoneNumber = PhoneNumber;
+            _street = Street;
+            _city = City;
+            _zipCode = ZipCode;
+            _country = Country;
+            _businessName = BusinessName;
+        }
+
+        public BusinessClient() { }
+    }
+}
