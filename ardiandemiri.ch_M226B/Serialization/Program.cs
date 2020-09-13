6c69afc4039fe45dfd531b4c1a2c92ff8f2362c4
Ardian Demiri <ademiri@online.gibz.ch>
2020-09-13 14:19:55 +0200

    * Program.cs: * Program.cs: * Serialize2.csproj: * Serialization.csproj: * AssemblyInfo.cs: * AssemblyInfo.cs:
    * ardiandemiri.ch_M226B.sln: Versionskontrolle

---
+++ ardiandemiri.ch_M226B/Serialization/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Serialization
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Ardian";
+            person.LastName = "Demiri";
+
+            string filePath = "data.save";
+
+            DataSerializer dataSerializer = new DataSerializer();
+
+            Person person2 = new Person();
+
+            dataSerializer.BinarySerialize(person, filePath);
+
+            person2 = dataSerializer.BinaryDeserialize(filePath) as Person;
+
+            Console.WriteLine(person2.FirstName);
+            Console.WriteLine(person2.LastName);
+
+
+
+        }
+    }
+
+    class DataSerializer
+    {
+        public void BinarySerialize(object data, string filepath)
+        {
+            FileStream fileStream;
+            BinaryFormatter bf = new BinaryFormatter();
+            if (File.Exists(filepath)) File.Delete(filepath);
+            fileStream = File.Create(filepath);
+            bf.Serialize(fileStream, data);
+            fileStream.Close();
+        }
+
+        public object BinaryDeserialize(string filePath)
+        {
+            object obj = null;
+
+            FileStream fileStream;
+            BinaryFormatter bf = new BinaryFormatter();
+            if(File.Exists(filePath))
+            {
+                fileStream = File.OpenRead(filePath);
+                obj = bf.Deserialize(fileStream);
+                fileStream.Close();
+            }
+            return obj;
+        }
+    } 
+
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ ardiandemiri.ch_M226B/Serialize2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+namespace Serialize2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Ardian";
+            person1.LastName = "Demiri";
+            Person person2 = new Person();
+
+            DataTransfer DT = new DataTransfer();
+            string filePath = "objFile.save";
+            DT.Serialize(person1, filePath);
+            person2 = DT.Deserialize(filePath) as Person;
+            Console.WriteLine("My Name ist {0} {1}", person2.FirstName, person2.LastName);
+        }
+    }
+
+    public class DataTransfer
+    {
+        public void Serialize(object obj, string filePath)
+        {
+            FileStream fs;
+            BinaryFormatter bf = new BinaryFormatter();
+            if (File.Exists(filePath)) File.Delete(filePath);
+            fs = File.Create(filePath);
+            bf.Serialize(fs, obj);
+            fs.Close();
+        }
+
+        public Object Deserialize(string filePath)
+        {
+            Object obj = null;
+
+            FileStream fs;
+            BinaryFormatter bf = new BinaryFormatter();
+
+            if(File.Exists(filePath))
+            {
+                fs = File.OpenRead(filePath);
+                obj = bf.Deserialize(fs);
+                fs.Close();
+            }
+            return obj;
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
