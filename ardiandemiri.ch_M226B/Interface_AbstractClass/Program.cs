8f4ff352ce0ab726d99178d03884e929712ef652
Ardian Demiri <ademiri@online.gibz.ch>
2020-10-17 12:29:56 +0200

    * Program.cs: * AssemblyInfo.cs: * Interface_AbstractClass.csproj:
    * ardiandemiri.ch_M226B.sln: Changes for abstract class and interface

---
+++ ardiandemiri.ch_M226B/Interface_AbstractClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Interface_AbstractClass
+{
+    class MainClass
+    {
+        //Just for testing reasons
+        public static void Main(string[] args)
+        {
+            Tiger tiger = new Tiger();
+            tiger.makeSound();
+            tiger.run();
+        }
+
+        public abstract class Animal
+        {
+            // Method with no body
+            public abstract void makeSound();
+
+            // Method with body but not abstract
+            public void run()
+            {
+                Console.WriteLine("running....");
+            }
+        }
+
+        public class Tiger : Animal
+        {
+            public override void makeSound()
+            {
+                Console.WriteLine("Tiger Sound...");
+            }
+        }
+    }
+}
