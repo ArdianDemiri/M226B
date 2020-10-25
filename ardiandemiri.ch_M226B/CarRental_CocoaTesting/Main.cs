b9230b53797be237c86e510be9deacfece5231ff
Ardian Demiri <ademiri@online.gibz.ch>
2020-10-25 15:36:35 +0100

    * ardiandemiri.ch_M226B.sln: * Info.plist: * Car_Rental.csproj: * Main.cs: * Info.plist: * ViewController.cs: * Entitlements.plist: * ViewController.designer.cs: * AppDelegate.cs: * Main.storyboard: * ViewController.cs: * Car_Rental_Cocoa.csproj: * Contents.json: * Entitlements.plist: * ViewController.designer.cs: * Contents.json: * ViewController.designer.cs: * Contents.json: * CarRental_CocoaTesting.csproj: * Contents.json: * AppIcon-16.png: * AppIcon-32.png: * AppIcon-128.png: * AppIcon-512.png: * AppIcon-256.png: * AppIcon-16@2x.png: * AppIcon-32@2x.png: * AppIcon-512@2x.png: * AppIcon-256@2x.png: * AppIcon-128@2x.png: * Contents.json: * AppIcon-32.png: * AppIcon-16.png: * AppIcon-128.png: * AppIcon-256.png: * AppIcon-512.png: * AppIcon-16@2x.png: * AppIcon-32@2x.png: * AppIcon-128@2x.png: * AppIcon-256@2x.png: * AppIcon-512@2x.png: * Contents.json: * AppIcon-32.png: * AppIcon-16.png: * AppIcon-128.png: * AppIcon-512.png: * AppIcon-256.png: * AppIcon-32@2x.png: * AppIcon-16@2x.png: * AppIcon-512@2x.png: * AppIcon-128@2x.png: * AppIcon-256@2x.png:
    * autovermietung.csprojAssemblyReference.cache: testing cocoa app
      deligation

---
+++ ardiandemiri.ch_M226B/CarRental_CocoaTesting/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace CarRental_CocoaTesting
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
