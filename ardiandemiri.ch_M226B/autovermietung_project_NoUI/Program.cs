2bcb0f43fa90756088aa1933c5fe9f7316581b77
Ardian Demiri <ademiri@online.gibz.ch>
2021-01-25 11:50:09 +0100

    test am schluss

---
+++ ardiandemiri.ch_M226B/autovermietung_project_NoUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace autovermietung_project_NoUI
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
